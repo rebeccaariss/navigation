eee060a57b2d87d8478eaa1620794988afef752f
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-03-28 23:41:11 -0400

    Target nav items to change classes for CSS animations - to be refactored! Ensuring functionality first.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navs = [nav1, nav2, nav3, nav4, nav5];
 
 function toggleNav() {
   // Toggle menu bars open/closed
@@ -17,10 +18,35 @@ function toggleNav() {
     // Animate in - overlay:
     overlay.classList.remove('overlay-slide-left');
     overlay.classList.add('overlay-slide-right');
+    
+    // Animate in - nav items:
+    nav1.classList.remove('slide-out-1');
+    nav1.classList.add('slide-in-1');
+    nav2.classList.remove('slide-out-2');
+    nav2.classList.add('slide-in-2');
+    nav3.classList.remove('slide-out-3');
+    nav3.classList.add('slide-in-3');
+    nav4.classList.remove('slide-out-4');
+    nav4.classList.add('slide-in-4');
+    nav5.classList.remove('slide-out-5');
+    nav5.classList.add('slide-in-5');
+
   } else {
     // Animate out - overlay:
     overlay.classList.remove('overlay-slide-right');
     overlay.classList.add('overlay-slide-left');
+    
+    // Animate out - nav items:
+    nav1.classList.remove('slide-in-1');
+    nav1.classList.add('slide-out-1');
+    nav2.classList.remove('slide-in-2');
+    nav2.classList.add('slide-out-2');
+    nav3.classList.remove('slide-in-3');
+    nav3.classList.add('slide-out-3');
+    nav4.classList.remove('slide-in-4');
+    nav4.classList.add('slide-out-4');
+    nav5.classList.remove('slide-in-5');
+    nav5.classList.add('slide-out-5');
   }
 }
 
