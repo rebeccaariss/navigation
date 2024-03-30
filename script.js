76abfce77201e65512bed54ccf689cfc8f2a5bfa
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-03-29 20:23:48 -0400

    Refactor for fewer lines of code

--- script.js
+++ script.js
@@ -5,48 +5,35 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
-const navs = [nav1, nav2, nav3, nav4, nav5];
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Control animation:
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`);
+  });
+}
 
 function toggleNav() {
   // Toggle menu bars open/closed
-  // (change class animates hamburger menu)
+  // (change class animates hamburger menu):
   menuBars.classList.toggle('change');
   
   // Toggle: menu active
   overlay.classList.toggle('overlay-active');
   if (overlay.classList.contains('overlay-active')) {
     // Animate in - overlay:
-    overlay.classList.remove('overlay-slide-left');
-    overlay.classList.add('overlay-slide-right');
+    overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
     
     // Animate in - nav items:
-    nav1.classList.remove('slide-out-1');
-    nav1.classList.add('slide-in-1');
-    nav2.classList.remove('slide-out-2');
-    nav2.classList.add('slide-in-2');
-    nav3.classList.remove('slide-out-3');
-    nav3.classList.add('slide-in-3');
-    nav4.classList.remove('slide-out-4');
-    nav4.classList.add('slide-in-4');
-    nav5.classList.remove('slide-out-5');
-    nav5.classList.add('slide-in-5');
+    navAnimation('out', 'in'); // replacing 'out' with 'in'
 
   } else {
     // Animate out - overlay:
-    overlay.classList.remove('overlay-slide-right');
-    overlay.classList.add('overlay-slide-left');
+    overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
     
     // Animate out - nav items:
-    nav1.classList.remove('slide-in-1');
-    nav1.classList.add('slide-out-1');
-    nav2.classList.remove('slide-in-2');
-    nav2.classList.add('slide-out-2');
-    nav3.classList.remove('slide-in-3');
-    nav3.classList.add('slide-out-3');
-    nav4.classList.remove('slide-in-4');
-    nav4.classList.add('slide-out-4');
-    nav5.classList.remove('slide-in-5');
-    nav5.classList.add('slide-out-5');
+    navAnimation('in', 'out'); // replacing 'in' with 'out'
   }
 }
 
